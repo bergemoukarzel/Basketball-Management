28463041b1b6a24943f4f5c0143fc062c63a5cdc
bergemoukarzel <129981504+bergemoukarzel@users.noreply.github.com>
2023-04-14 11:05:02 +0300

    progress

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
-from .views  import PlayerList, TaskDetail
+from .views  import PlayerList, PlayerDetail, PlayerCreate
 urlpatterns = [
     path('', PlayerList.as_view(), name = 'BasketballPlayers'),
-    path('player/<int:pk>/', TaskDetail.as_view(), name='player'),
+    path('player/<int:pk>/', PlayerDetail.as_view(), name='player'),
+    path('player-create/', PlayerCreate.as_view(), name = 'player-create'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView 
+from django.urls import reverse_lazy
 from .models import Task
 
 class PlayerList(ListView):
     model = Task
-    context_object_name = 'Players'
+    context_object_name = 'players'
 
-class TaskDetail(DetailView):
+class PlayerDetail(DetailView):
     model = Task
     context_object_name = 'player'
     template_name = 'base/player.html'
+
+class PlayerCreate(CreateView):
+    model = Task
+    #field = ['title', 'description']
+    fields = '__all__'
+    success_url = reverse_lazy('players')
+    template_name = 'base/Player_form.html'
