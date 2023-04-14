61224ac711b3c903da375cbb6ebf77eee084b76b
bergemoukarzel <129981504+bergemoukarzel@users.noreply.github.com>
2023-04-14 12:41:53 +0300

    Delete, Edit, Add

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views  import PlayerList, PlayerDetail, PlayerCreate
+from .views  import PlayerList, PlayerDetail, PlayerCreate, PlayerUpdate, PlayerDelete
 urlpatterns = [
     path('', PlayerList.as_view(), name = 'BasketballPlayers'),
-    path('player/<int:pk>/', PlayerDetail.as_view(), name='players'),
+    path('player/<int:pk>/', PlayerDetail.as_view(), name='player'),
     path('player-create/', PlayerCreate.as_view(), name = 'player-create'),
+    path('player-update/<int:pk>/', PlayerUpdate.as_view(), name='player-update'),
+    path('player-delete/<int:pk>/', PlayerDelete.as_view(), name='player-delete'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Task
 
@@ -18,5 +18,18 @@ class PlayerCreate(CreateView):
     model = Task
     #field = ['title', 'description']
     fields = '__all__'
-    success_url = reverse_lazy('players')
+    success_url = reverse_lazy('BasketballPlayers')
+    #or players
     template_name = 'base/Player_form.html'
+
+class PlayerUpdate(UpdateView):
+    model = Task
+    fields ='__all__'
+    success_url = reverse_lazy('BasketballPlayers')
+    template_name = 'base/Player_form.html'
+
+class PlayerDelete(DeleteView):
+    model = Task
+    context_object_name = 'player'
+    template_name = 'base/player_confirm_delete.html'
+    success_url = reverse_lazy('BasketballPlayers')
