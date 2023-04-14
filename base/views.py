014f73997a34801acba4750709be3f55c1432cb0
bergemoukarzel <129981504+bergemoukarzel@users.noreply.github.com>
2023-04-14 15:11:57 +0300

    Login/Logout

--- base/views.py
+++ base/views.py
@@ -10,10 +10,8 @@ class CustomLoginView(LoginView):
     template_name = 'base/login.html'
     fields = '__all__'
     redirect_authenticated_user = True
-    success_url = reverse_lazy('BasketballPlayers')
-
-    #def get_success_url(self):
-     #   return reverse_lazy('BasketballPlayers')
+    def get_success_url(self):
+      return reverse_lazy('BasketballPlayers')
 
 
 class PlayerList(ListView):
