f0b86d8b8907908b6d096fe646737ec381eeb15d
bergemoukarzel <129981504+bergemoukarzel@users.noreply.github.com>
2023-04-14 15:10:32 +0300

    Login/Logout

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views  import PlayerList, PlayerDetail, PlayerCreate, PlayerUpdate, PlayerDelete
+from .views  import PlayerList, PlayerDetail, PlayerCreate, PlayerUpdate, PlayerDelete, CustomLoginView
+from django.contrib.auth.views import LogoutView
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page= 'login'), name='logout'),
     path('', PlayerList.as_view(), name = 'BasketballPlayers'),
     path('player/<int:pk>/', PlayerDetail.as_view(), name='player'),
     path('player-create/', PlayerCreate.as_view(), name = 'player-create'),
